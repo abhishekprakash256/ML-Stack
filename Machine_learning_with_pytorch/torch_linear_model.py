1316ba54d039d3a67384f439a8f6a742c2214aa4
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-10-07 15:15:47 -0400

    prediction from model

--- Machine_learning_with_pytorch/torch_linear_model.py
+++ Machine_learning_with_pytorch/torch_linear_model.py
@@ -16,90 +16,79 @@ th.manual_seed(5)
 
 
 class Data_handling():
-    """
-    A class for data handling operations, including data generation and train-test splitting.
+	"""
+	A class for data handling operations, including data generation and train-test splitting.
 
-    Attributes:
-    X (Tensor): Input data tensor.
-    Y (Tensor): Target data tensor.
-    X_train (Tensor): Input data for training.
-    y_train (Tensor): Target data for training.
-    X_test (Tensor): Input data for testing.
-    y_test (Tensor): Target data for testing.
+	Methods:
+	make_data(): Generates synthetic data with specified start, end, and step values.
+	train_test_split(): Splits the generated data into training and testing sets.
+	"""
+	
+	def make_data(self):
+		"""
+		Generate synthetic data.
 
-    Methods:
-    make_data(): Generates synthetic data with specified start, end, and step values.
-    train_test_split(): Splits the generated data into training and testing sets.
-    """
-    
-    def make_data(self):
-        """
-        Generate synthetic data.
+		Generates synthetic data using specified start, end, and step values.
+		The data consists of input values (X) and corresponding target values (Y),
+		calculated based on WEIGHT and BIAS.
 
-        Generates synthetic data using specified start, end, and step values.
-        The data consists of input values (X) and corresponding target values (Y),
-        calculated based on WEIGHT and BIAS.
+		Returns:
+		None
+		"""
+		start = 0
+		end = 2 
+		step = 0.02
 
-        Returns:
-        None
-        """
-        start = 0
-        end = 2 
-        step = 0.02
+		self.X = th.arange(start, end, step).unsqueeze(dim=1)
+		self.Y = WEIGHT * self.X + BIAS
+	
+	def train_test_split(self):
+		"""
+		Split the generated data into training and testing sets.
 
-        self.X = th.arange(start, end, step).unsqueeze(dim=1)
-        self.Y = WEIGHT * self.X + BIAS
-    
-    def train_test_split(self):
-        """
-        Split the generated data into training and testing sets.
+		Splits the generated data into training and testing sets based on a predefined split ratio (80% train, 20% test).
 
-        Splits the generated data into training and testing sets based on a predefined split ratio (80% train, 20% test).
+		Returns:
+		None
+		"""
+		train_split = int(0.8 * len(self.X))
 
-        Returns:
-        None
-        """
-        train_split = int(0.8 * len(self.X))
-
-        self.X_train, self.y_train = self.X[:train_split], self.Y[:train_split]
-        self.X_test, self.y_test = self.X[train_split:], self.Y[train_split:]
+		self.X_train, self.y_train = self.X[:train_split], self.Y[:train_split]
+		self.X_test, self.y_test = self.X[train_split:], self.Y[train_split:]
 
 
 
 class Linear_Model(nn.Module):
-    """
-    A simple linear regression model implemented as a PyTorch module.
+	"""
+	A simple linear regression model implemented as a PyTorch module.
 
-    Attributes:
-    linear_layer (nn.Linear): The linear transformation layer of the model.
+	Methods:
+	forward(x): Performs a forward pass through the model.
+	"""
 
-    Methods:
-    forward(x): Performs a forward pass through the model.
-    """
+	def __init__(self):
+		"""
+		Initialize the Linear_Model.
 
-    def __init__(self):
-        """
-        Initialize the Linear_Model.
+		Creates a linear transformation layer with one input feature and one output feature.
 
-        Creates a linear transformation layer with one input feature and one output feature.
+		Returns:
+		None
+		"""
+		super().__init__()
+		self.linear_layer = nn.Linear(in_features=1, out_features=1)
 
-        Returns:
-        None
-        """
-        super().__init__()
-        self.linear_layer = nn.Linear(in_features=1, out_features=1)
+	def forward(self, x):
+		"""
+		Forward pass through the linear regression model.
 
-    def forward(self, x):
-        """
-        Forward pass through the linear regression model.
+		Args:
+		x (Tensor): Input tensor.
 
-        Args:
-        x (Tensor): Input tensor.
-
-        Returns:
-        Tensor: Output tensor produced by the linear regression model.
-        """
-        return self.linear_layer(x)
+		Returns:
+		Tensor: Output tensor produced by the linear regression model.
+		"""
+		return self.linear_layer(x)
 
 
 class Test_and_train:
@@ -162,8 +151,19 @@ if __name__ == "__main__":
 	model = Linear_Model()
 	model.load_state_dict(param)
 
-	
+
+	#get the data sample
+
+	sample_data = Data_handling()
+
+	sample_data.make_data()
+
+	sample_data_x = sample_data.X[0:10]
+	sample_data_y = sample_data.Y[0:10]
+
+	y_pred = model(sample_data_x)
 
 
-
-
+	#make the data predition on the model
+	print(y_pred)
+	print(sample_data_y)
