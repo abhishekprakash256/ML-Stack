cc08bd227a7b79c650321db7bae02cb069cf6d7d
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-10-09 23:26:56 -0400

    mongo code

--- Machine_learning_with_pytorch/mongodb_storage.py
+++ Machine_learning_with_pytorch/mongodb_storage.py
@@ -8,3 +8,23 @@ import torch as th
 from pymongo import MongoClient
 
 
+import pymongo
+
+# Replace these with your MongoDB connection details
+mongo_uri = "mongodb://localhost:27017"  # Connection URI
+database_name = ""      # Database name
+collection_name = ""        # Name of the new collection
+
+# Connect to MongoDB
+client = pymongo.MongoClient(mongo_uri)
+
+# Access the database
+db = client[sample_data]
+
+# Create a new collection
+db.create_collection(test_data)
+
+# Close the MongoDB connection when done
+client.close()
+
+print(f"Collection '{collection_name}' created successfully.")
