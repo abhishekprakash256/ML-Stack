09ed8c1f3a962977580cf1a455993e914550c534
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-10-10 20:49:32 +0000

    data chopping

--- Machine_learning_with_pytorch/exp.py
+++ Machine_learning_with_pytorch/exp.py
@@ -4,9 +4,9 @@ The experiment to load the data from an array
 
 import torch as th 
 
-x = th.rand(10)
+x = th.rand(50)
 print(x)
 
-for i in range(2):
+for i in range(10):
 
     print(x[i*5:(i+1)*5])
