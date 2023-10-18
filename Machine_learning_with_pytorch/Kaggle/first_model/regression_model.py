a98b7a3314e54febeadf8cae1d67fe744a88ee1c
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-10-18 21:09:33 +0000

    kaggle code

--- Machine_learning_with_pytorch/Kaggle/first_model/regression_model.py
+++ Machine_learning_with_pytorch/Kaggle/first_model/regression_model.py
@@ -1,3 +1,15 @@
 """
 make the regression model 
 """
+import pandas as pd 
+import torch as th 
+
+
+
+DATA_SET_PATH = "../datasets/Melbourne_housing_FULL.csv"
+
+
+
+df = pd.read_csv(DATA_SET_PATH)
+
+print(df.describe())
