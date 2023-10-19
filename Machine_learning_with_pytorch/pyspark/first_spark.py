0932aebb124f37be54bc35e5e639ab7140edc372
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-10-19 01:04:43 -0400

    spark code

--- Machine_learning_with_pytorch/pyspark/first_spark.py
+++ Machine_learning_with_pytorch/pyspark/first_spark.py
@@ -1,3 +1,26 @@
 """
 using  pyspark for data manipulation 
 """
+from pyspark.sql import SparkSession
+import pandas as pd
+
+
+
+
+spark = SparkSession.builder.appName("Practise").getOrCreate()
+
+#print(spark)
+
+df_pyspark = spark.read.option('header','true').csv('test-sheet.csv')
+
+
+print(df_pyspark.columns)
+
+print(df_pyspark.dtypes)
+
+print(df_pyspark.describe())
+
+print(df_pyspark.describe().show())
+
+print(df_pyspark.head(2))
+
