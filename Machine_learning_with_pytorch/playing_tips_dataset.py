b7241aafd87df818d0c7504e5568e7ff54ae2ac1
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-10-19 21:04:44 +0000

    new code

--- Machine_learning_with_pytorch/playing_tips_dataset.py
+++ Machine_learning_with_pytorch/playing_tips_dataset.py
@@ -6,3 +6,28 @@ import pandas as pd
 import torch as th 
 
 
+#data file path
+FILE_PATH = "./datasets/tips.csv"
+
+
+class Data:
+
+    def __init__(self):
+        self.df = df
+        self.X = X
+        self.y = y
+
+
+    def make_data(self):
+        """
+        get the data and split in the data in X and y 
+        """
+        self.df = pd.read_csv(FILE_PATH)
+
+
+
+
+
+if __name__ == "__main__":
+    data = Data()
+    
