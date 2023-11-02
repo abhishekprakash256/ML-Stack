8bf6cd6ef523b77fd0d9a218e14e4f23d08bba35
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-11-02 20:13:29 +0000

    sklearn files

--- sklearn/intro_sk.py
+++ sklearn/intro_sk.py
@@ -8,6 +8,29 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 import matplotlib.pylab as plt
+import pandas as pd
 
 
+"""
 
+
+mod = KNeighborsRegressor()
+
+mod.fit(X,y)
+
+pred = mod.predict(X)
+
+print(pred)
+
+"""
+
+X,y = fetch_california_housing(return_X_y=True)
+
+pipe = Pipeline([("scale",StandardScaler()),("model", KNeighborsRegressor(n_neighbors =1 ))])
+
+
+mod = GridSearchCV(estimator = pipe,param_grid = {'model__n_neighbors':[1,2,3,4,5,6]},cv =3)
+
+mod.fit(X,y)
+
+print(pd.DataFrame(mod.cv_results_))
