35daed4dd5582e69cfd1ba9e6ffb45cd71b470ed
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-11-04 17:28:36 -0400

    data handling

--- Hands_on_mahine_learning/Ch-2/ml_full.py
+++ Hands_on_mahine_learning/Ch-2/ml_full.py
@@ -1,17 +1,41 @@
 """
 Using the california housing prices dataset 
 """
-
+#imports 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+
 
 
 #FILE PATH
 FILE_PATH = "/home/abhi/Datasets/housing.csv"
 
 
-df = pd.read_csv(FILE_PATH)
+class Data():
+    def __init__(self):
+        self.df = None
+        self.X = None
+        self.y = None
+        self.x_train = None
+        self.x_test = None
+        self.y_train = None
+        self.y_test = None
+    
+
+    def make_data(self):
+        """
+        The function to make thge data set
+        """
+
+        self.df = pd.read_csv(FILE_PATH)
 
 
-print(df.head())
-print(df.info())
+
+
+
+if __name__ == "__main__":
+    data = Data()
+    data.make_data()
