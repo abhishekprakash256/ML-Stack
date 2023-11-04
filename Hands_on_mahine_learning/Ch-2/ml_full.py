c9a15807587220dc69c562ec5a56f207e53e9f3a
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-11-04 16:57:15 -0400

    load data

--- Hands_on_mahine_learning/Ch-2/ml_full.py
+++ Hands_on_mahine_learning/Ch-2/ml_full.py
@@ -1,3 +1,17 @@
 """
 Using the california housing prices dataset 
 """
+
+import pandas as pd
+import numpy as np
+
+
+#FILE PATH
+FILE_PATH = "/home/abhi/Datasets/housing.csv"
+
+
+df = pd.read_csv(FILE_PATH)
+
+
+print(df.head())
+print(df.info())
