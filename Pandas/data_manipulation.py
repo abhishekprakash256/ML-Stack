b8c6b718c0abb70b211882b9bfb8c1a2771c24dc
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-11-07 20:25:18 +0000

    dask approach

--- Pandas/data_manipulation.py
+++ Pandas/data_manipulation.py
@@ -2,8 +2,12 @@
 pandas to maipulate the file
 """
 import pandas as pd 
-
+import dask.dataframe as dd
 
 FILE_PATH = "/home/ubuntu/s3/ihateabhi.csv"
 
-df = pd.read_csv(FILE_PATH, encoding='ISO-8859-1')
+df = dd.read_csv(FILE_PATH)
+
+
+print(df.info())
+#df = pd.read_csv(FILE_PATH, encoding='ISO-8859-1')
