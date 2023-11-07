c183f16f1187edf5786b5185409b4782e8a836e5
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-11-07 20:03:23 +0000

    pandas practice

--- Pandas/intro_pd.py
+++ Pandas/intro_pd.py
@@ -1,3 +1,32 @@
 """
-
+pandas to maipulate the file
 """
+import pandas as pd 
+
+
+FILE_PATH = "/home/ubuntu/s3/creditcard.csv"
+
+df = pd.read_csv(FILE_PATH)
+
+#print the column of dataset
+print(df.Amount)
+
+#print the Amount columns
+print(df["Amount"])
+
+#print the value in value in the column 
+print(df["Amount"][0])
+
+#using the iloc 
+print(df.iloc[0])
+
+
+#using the loc 
+print(df.loc[0])
+
+
+#using the column as target
+print(df.loc[0,'Amount'])
+
+#set the index in the 
+print(df.set_index("Amount"))
