afef4772e0d70b76184023d296b00b706b67fb70
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-11-08 00:00:59 -0500

    pandas handlking

--- Pandas/data_manipulation.py
+++ Pandas/data_manipulation.py
@@ -4,10 +4,10 @@ pandas to maipulate the file
 import pandas as pd 
 import dask.dataframe as dd
 
-FILE_PATH = "/home/ubuntu/s3/ihateabhi.csv"
+FILE_PATH_1 = "/home/ubuntu/s3/ihateabhi.csv"
+FILE_PATH_2 = "/home/abhi/Datasets/ihateabhi.csv"
 
-df = dd.read_table(FILE_PATH,encoding='ISO-8859-1')
+df = pd.read_csv(FILE_PATH_1,encoding='ISO-8859-1')
 
 
 print(df.head(0))
-#df = pd.read_csv(FILE_PATH, encoding='ISO-8859-1')
