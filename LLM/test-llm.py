373532d40a9247cd0a8178425f0f77e73dd93b75
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-06-17 01:12:14 -0400

    code

--- LLM/test-llm.py
+++ LLM/test-llm.py
@@ -2,16 +2,19 @@
 make the testin of the LLM model 
 """
 
-
 from transformers import pipeline
 
-
+# Create the sentiment analysis pipeline
 sentiment_pipeline = pipeline("sentiment-analysis")
-#data = ["I love you", "I hate you"]
 
+# Example data
+data = "there is a deer that lives in the far mountains, I don't like the mountain but the deer is good to eat, I tried to kill the deer but it attacked me and I got injured but I am not injured much as I defended myself well"
 
-data = "there is a deer that lives in the far mountains, I don't like the mountain but the deer is good to eat, I tried to kill the dear but it attacked me and I got injured but I am not injured much as I defended myself well"
+# Run the sentiment analysis
+result = sentiment_pipeline(data)
 
+# Print the result
+print(result)
 
-print(sentiment_pipeline(data))
-
+# Print out the model information
+print(sentiment_pipeline.model)
